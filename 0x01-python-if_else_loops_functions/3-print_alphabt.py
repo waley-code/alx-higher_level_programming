7f53d9b9488ffa52d07fe14cb0ba1866cd31f985
waley-code <oyewaleadeosun@yahoo.com>
2022-11-29 16:24:15 -0800

    task 4

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(97, 123):
     if f"{i:c}" == 'q' or f"{i:c}" == 'e':
-         continue
+        continue
     else:
-         print("{:c}".format(i), end="")
+        print("{:c}".format(i), end="")
