8efd6d44e81dfe4f31eb7016c2abdcdab16d5f68
waley-code <oyewaleadeosun@yahoo.com>
2022-11-29 18:12:52 -0800

    task 8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    stri =""
+    for i in str:
+        if i >= 'a' and i <= 'z':
+            stri += "{:c}".format(ord(i)-32)
+        else:
+            stri += "{}".format(i)
+    print(stri, end="\n")
