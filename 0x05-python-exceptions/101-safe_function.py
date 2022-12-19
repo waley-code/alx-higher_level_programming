4e5a226d3280a3abccc6baaeb95bdb026db47c55
waley-code <oyewaleadeosun@yahoo.com>
2022-12-19 20:06:36 +0000

    task 101

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import sys
+def safe_function(fct, *args):
+    try:
+        result = fct(args[0], args[1])
+    except Exception as n:
+         print(f"Exception: {n}", file=sys.stderr)
+    else:
+        return result
