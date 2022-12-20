24be0c56bea211cd02025b5aa94ba1773a52a654
waley-code <oyewaleadeosun@yahoo.com>
2022-12-20 16:18:44 +0000

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+    This is a module containing a class that defines a class square.
+"""
+class Square:
+    """
+        This class defines class when instantiated.
+    """
+    def __init__(self, size=0):
+        """
+        Initializes private size value
+        """
+        self.__size = size
