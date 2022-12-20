2bf1245e0b3df3ce68eb0b3e44e35c9b283b762e
waley-code <oyewaleadeosun@yahoo.com>
2022-12-20 20:47:36 +0000

    task 06

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -83,18 +83,3 @@ class Square:
             for i in range(self.__size):
                 print("{}".format(self.__position[0] * "_") , end="")
                 print("{}".format(self.__size * "#") )
-
-my_square_1 = Square(3)
-my_square_1.my_print()
-
-print("--")
-
-my_square_2 = Square(3, (1, 1))
-my_square_2.my_print()
-
-print("--")
-
-my_square_3 = Square(3, (3, 0))
-my_square_3.my_print()
-
-print("--")
