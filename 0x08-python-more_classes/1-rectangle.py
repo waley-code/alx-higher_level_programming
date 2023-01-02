88e97ae565ede50456b3f1c78dc9e151a748e279
waley-code <oyewaleadeosun@yahoo.com>
2023-01-02 15:34:21 +0000

    task 1

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+This is the "Rectangle"  module.
+This module provides a simple Rectangle class.
+"""
+
+
+class Rectangle:
+    """ defines a rectangle """
+    def __init__(self, width=0, height=0):
+        """Initializes width"""
+        self.__width = width
+        self.__height = height
+    
+    @property
+    def width(self):
+        """returns width"""
+        return self.__width
+    
+    @property
+    def height(self):
+        """returns height"""
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        """sets width private property"""
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+    
+    @height.setter
+    def height(self, value):
+        """sets height private property"""
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
