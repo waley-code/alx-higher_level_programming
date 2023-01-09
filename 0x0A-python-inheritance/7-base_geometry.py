d6a0c9761a54e8d1127636b7241f28c7f47e156d
waley-code <oyewaleadeosun@yahoo.com>
2023-01-09 15:02:48 +0000

    task 07

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -5,7 +5,7 @@
 """
 
 
-class BaseGeometry(object):
+class BaseGeometry:
     """ a BaseGeometry class inherit from object"""
 
     def area(self):
@@ -19,6 +19,7 @@ class BaseGeometry(object):
         """Method that validates value"""
 
         if type(value) is not int:
-            raise TypeError("{:s} must be an integer".format(name))
+            raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise ValueError("{:s} must be greater than 0".format(name))
+            raise ValueError("{} must be greater than 0".format(name))
+        return
