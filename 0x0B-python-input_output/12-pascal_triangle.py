252b33916a0e365e6a31d11315c14647493a16f3
waley-code <oyewaleadeosun@yahoo.com>
2023-01-10 22:37:03 +0000

    tsask 12

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module of pascals triangle"""
+
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+    x = [1]
+    for row_num in range(n):
+        print([*x])
+        previous = x[:]
+        for k in range(row_num):
+            x[k + 1] = previous[k] + previous[k + 1]
+        x.append(1)
