d690a1972583aa67a13ca2fc6580386bc0967df4
waley-code <oyewaleadeosun@yahoo.com>
2023-01-10 15:30:15 +0000

    task 4

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""module contains a function that returns
+    an object (Python data structure)
+    represented by a JSON string:
+"""
+import json
+
+
+def from_json_string(my_str):
+    """
+    returns object represented by a JSON string
+    """
+    return json.loads(my_str)
