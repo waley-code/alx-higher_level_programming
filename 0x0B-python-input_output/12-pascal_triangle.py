743c2a2e82f92557726fd3944f1d4ecdaa3e6032
waley-code <oyewaleadeosun@yahoo.com>
2023-01-12 05:57:55 +0000

    task 12

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
+"""Module of pascals triangle"""
+
+
 def pascal_triangle(n):
-    """ Pascal Triangle 1h"""
-    li = []
     if n <= 0:
-        return li
-    for r in range(n):
-        for c in range(r + 1):
-            if c == 0:
-                li.append([1])
-            elif c == r:
-                li[r].append(1)
-            else:
-                li[r].append(li[r - 1][c] + li[r - 1][c - 1])
-    return li
+        return []
+    x = [1]
+    for row_num in range(n):
+        for i in x:
+            print(i, end="")
+        print()
+        previous = x[:]
+        for k in range(row_num):
+            x[k + 1] = previous[k] + previous[k + 1]
+        x.append(1)
+
+
+pascal_triangle(5)
