10d346e6610d78451bfdda6e2989e291939dc986
waley-code <walephlp@gmail.com>
2023-02-15 22:55:31 +0000

    task 8

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,3 @@
+-- script that creates the table force_name on your MySQL server.
+-- creates the force_name table with two columns: id with data type INT and name with data type VARCHAR(256). 
+CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
