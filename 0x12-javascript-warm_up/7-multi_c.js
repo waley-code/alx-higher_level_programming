9e73a73c5fadf39bf53f227dd27bc22d6aa697d4
waley-code <walephlp@gmail.com>
2023-03-13 14:24:15 +0000

    complete task 7

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const args = process.argv.slice(2);
+
+if (Number.isInteger(parseInt(args[0]))) {
+  for (let index = 0; index < args[0]; index++) {
+    console.log('C is fun');
+  }
+} else {
+  console.log('Missing number of occurrences');
+}
