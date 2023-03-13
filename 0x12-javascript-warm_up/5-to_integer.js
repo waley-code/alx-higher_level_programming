4b3534d37078d4783c8029fe05385385b472f595
waley-code <walephlp@gmail.com>
2023-03-13 14:23:04 +0000

    complete task 6

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const args = process.argv.slice(2);
+
+if (Number.isInteger(parseInt(args[0]))) {
+  console.log('My number: ' + Math.floor(args[0]));
+} else {
+  console.log('Not a number');
+}
---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const myArr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+myArr.forEach(arr => {
+  console.log(arr);
+});
