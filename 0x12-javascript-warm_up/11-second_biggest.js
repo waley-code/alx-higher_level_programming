77d043c5c4fc0e6b8bff951d67568e5236fc1d3c
waley-code <walephlp@gmail.com>
2023-03-13 14:27:43 +0000

    completed task 11

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const args = process.argv.slice(2);
+let num = 0;
+let prev = 0;
+
+if (args.length <= 1) {
+  console.log(prev);
+} else {
+  args.forEach(el => {
+    if (el > num) {
+      prev = num;
+      num = el;
+    }
+  });
+  console.log(prev);
+}
