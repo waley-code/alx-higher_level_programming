df15f5339b3cd133676f1d1d7c05b033416e6e6b
waley-code <walephlp@gmail.com>
2023-03-13 14:24:32 +0000

    completed task 8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const args = process.argv.slice(2);
+
+if (Number.isInteger(parseInt(args[0]))) {
+  for (let index = 0; index < args[0]; index++) {
+    let x = '';
+    for (let index = 0; index < args[0]; index++) {
+      x += 'X';
+    }
+    console.log(x);
+  }
+} else {
+  console.log('Missing size');
+}
