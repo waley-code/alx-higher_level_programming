7cfb84629c15d267c048149acd4da8862866c620
waley-code <walephlp@gmail.com>
2023-03-13 14:25:32 +0000

    completed task 10

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const args = process.argv.slice(2);
+const first = parseInt(args[0]);
+
+function add (a) {
+  if (isNaN(a)) {
+    return 1;
+  } else if (a === 0) {
+    return 1;
+  } else {
+    return a * add(a - 1);
+  }
+}
+
+console.log(add(first));
