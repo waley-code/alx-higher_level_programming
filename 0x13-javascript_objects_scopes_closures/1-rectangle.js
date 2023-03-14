511566923b7b55630611e69d1094d2162ac8b581
waley-code <walephlp@gmail.com>
2023-03-14 17:31:23 +0000

    complete task 1

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+class Rectangle {
+  // an empty class Rectangle that defines a rectangle:
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+
+module.exports = Rectangle;
