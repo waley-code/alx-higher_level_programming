12d947c6aa705db21d3125ac83c99922d5767716
waley-code <walephlp@gmail.com>
2023-03-18 08:41:29 +0000

    complete relationship_state task 100

---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,20 @@
+""" a python file that contains the class definition of a State class
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+class State(Base):
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    from relationship_city import City
+    cities = relationship("City", cascade="all, delete", backref="state")
+
+    def __init__(self, name):
+        self.name = name
