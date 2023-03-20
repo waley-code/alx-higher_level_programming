ee55b17935d4832533ba2b69d744d860e39c97b0
waley-code <walephlp@gmail.com>
2023-03-20 07:23:08 +0000

    fix class documentation issues

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""a script that creates the State “California” with the"""
+"""a script that creates the State California with the"""
 
 import sys
 from sqlalchemy import (create_engine)
