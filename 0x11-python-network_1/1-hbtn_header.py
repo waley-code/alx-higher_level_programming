8c20040d23124b6fe942e210a602cbcf8e89190f
waley-code <walephlp@gmail.com>
2023-03-31 22:07:19 +0000

    1-hbtn_header.py

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# Python script that fetches header from https://alx-intranet.hbtn.io/status
+from urllib import request
+import sys
+
+# Open a request and receiving response
+with request.urlopen(sys.argv[1]) as rep:
+    # getting X-Request-Id value from header
+    req_id = rep.getheader('X-Request-Id')
+# Outpuy the value to screen
+print(req_id)
