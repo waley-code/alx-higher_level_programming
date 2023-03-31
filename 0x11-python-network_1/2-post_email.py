aaebca1e4b08a3ee352a3dfd7fadf900bdd9b061
waley-code <walephlp@gmail.com>
2023-03-31 22:07:33 +0000

    2-post_email.py

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# script that takes in a URL and an email, sends a POST request
+from urllib import request, parse
+import sys
+
+mail = parse.urlencode({'email': sys.argv[2]}).encode('utf-8')
+# Sending a post request
+with request.urlopen(sys.argv[1], mail) as rep:
+    response_bd = rep.read().decode('utf-8')
+print(response_bd)
