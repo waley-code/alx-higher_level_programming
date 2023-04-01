12ce18192f0bb56f5d069a1def1e2beb9540f180
waley-code <walephlp@gmail.com>
2023-04-01 10:11:28 +0000

    10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-"""script that takes your GitHub credentials (username and password)"""
+"""script that takes your GitHub credentials
+    (username and password)
+"""
 import sys
 import requests
 
 if __name__ == "__main__":
-    rep = requests.get("https://api.github.com/user",\
-            auth=(sys.argv[1], sys.argv[2] ))
+    rep = requests.get("https://api.github.com/user",
+                       auth=(sys.argv[1], sys.argv[2]))
     if rep.status_code == 200:
         data = rep.json()
         print(data["id"])
-
