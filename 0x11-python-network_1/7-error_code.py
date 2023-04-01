5a802a34bbe69b04fa958e8f524e33c102593e0b
waley-code <walephlp@gmail.com>
2023-04-01 00:10:21 +0000

    7-error_code.py

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,10 @@
+##!/usr/bin/python3
+# script that takes in a URL, sends a request to the URL
+import sys
+import requests
+
+rep = requests.get(sys.argv[1])
+if rep.status_code >= 400:
+    print(f"Error code: {rep.status_code}")
+else:
+    print(rep.content.decode('utf-8'))
