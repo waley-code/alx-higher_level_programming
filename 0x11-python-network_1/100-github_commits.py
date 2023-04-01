d7108f1a03469bef031fd3e200a8f3dc74ce0d8d
waley-code <oyewaleadeosun@yahoo.com>
2023-04-01 08:09:47 -0700

    completed advanced task

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Python script that takes 2 arguments in order
+    to solve this challenge.
+"""
+from sys import argv
+import requests
+
+if __name__ == "__main__":
+    ur = f"https://api.github.com/repos/{argv[2]}/{argv[1]}/commits"
+    rep = requests.get(ur)
+    if rep.status_code == 200:
+        com_msg = rep.json()
+        for com in range(10):
+            sha = com_msg[com].get("sha")
+            name = com_msg[com]['commit']['author']['name']
+            print(f"{sha}: {name}")
+    else:
+        print("Error while trying to retrieve commits.\
+                Error: {}".format(rep.status_code))
