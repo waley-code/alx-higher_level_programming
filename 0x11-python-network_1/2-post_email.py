5f6fb16f9bed9399363d70526cb8f4ecb53c3e72
waley-code <walephlp@gmail.com>
2023-04-01 00:11:51 +0000

    2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -4,7 +4,9 @@ from urllib import request, parse
 import sys
 
 mail = parse.urlencode({'email': sys.argv[2]}).encode('utf-8')
+
 # Sending a post request
 with request.urlopen(sys.argv[1], mail) as rep:
     response_bd = rep.read().decode('utf-8')
+
 print(response_bd)
