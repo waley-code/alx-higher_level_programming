f5f2474bc8d43c9646520a98a63c3b386d6f69b0
waley-code <walephlp@gmail.com>
2023-04-01 09:55:41 +0000

    4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,5 +5,5 @@ import requests
 if __name__ == "__main__":
     rep = requests.get('https://alx-intranet.hbtn.io/status')
     print("Body response:")
-    print(f"\t- type {type(rep.text)}")
+    print(f"\t- type: {type(rep.text)}")
     print(f"\t- content: {rep.text}")
